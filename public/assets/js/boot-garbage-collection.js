8603b2f742a9b132dc8b14f4996bc33ad994cd5a
vinso0 <arvinsocao2005@gmail.com>
2025-10-28 12:27:57 +0800

    Updated

--- public/assets/js/boot-garbage-collection.js
+++ public/assets/js/boot-garbage-collection.js
@@ -1,4 +1,10 @@
 // Remove all import statements and use global classes
+console.log('🔍 Debug: Script loading check');
+console.log('L (Leaflet):', typeof L);
+console.log('RouteMapSelector:', typeof window.RouteMapSelector);
+console.log('AddTruckView:', typeof window.AddTruckView);
+console.log('EditTruckView:', typeof window.EditTruckView);
+
 document.addEventListener('DOMContentLoaded', () => {
   // Classes will be loaded as globals from individual script tags
   if (window.AddTruckView) new AddTruckView('addTruckModal', 'addRouteMap');
